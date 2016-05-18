8ebc04e11498606a13199d0436bd743e8d67ddbb
christiearcus <christie.arcus@gmail.com>
2016-05-18 17:06:27 +1000

    creating a stripe card token and saving to database

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/' => 'tickets#index'
 
   get '/tickets' => 'tickets#tickets'
+  post '/tickets/submit' => 'tickets#submit'
 
   get '/sponsors' => 'sponsors#sponsors'
 
