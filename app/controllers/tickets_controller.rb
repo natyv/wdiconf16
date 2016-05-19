2a2e2c6d0eb10d34784a302bedf6d1ba996c1d84
Pranati Dave <pranati.dave@gmail.com>
2016-05-19 17:39:36 +1000

    sorting table

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,9 +1,10 @@
 require 'stripe'
 
 class TicketsController < ApplicationController
-
+  helper_method :sort_column, :sort_direction
   def index
-    @sponsors = Sponsor.all
+    @sponsors = Sponsor.order("#{sort_column} #{sort_direction}")
+    # @sponsors = Sponsor.all
   end
 
   def tickets
@@ -32,5 +33,19 @@ class TicketsController < ApplicationController
       # The card has been declined
     end
   end
+
+
+  private
+    def sortable_columns
+      [ "amount"]
+    end
+
+    def sort_column
+      sortable_columns.include?(params[:column]) ? params[:column] : "amount"
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+    end
 
 end
