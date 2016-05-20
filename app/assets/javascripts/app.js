f58f4210c7bebf87b1bead3ed86c9214a2883244
isha <isha.negi19@gmail.com>
2016-05-20 16:08:51 +1000

    resolving conflict

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,30 +1,45 @@
-$(function() {
-  var $form = $('#payment-form');
-  $form.submit(function(event) {
-    // Disable the submit button to prevent repeated clicks:
-    $form.find('.submit').prop('disabled', true);
-    // Request a token from Stripe:
-    Stripe.card.createToken($form, stripeResponseHandler);
-    // Prevent the form from being submitted:
-    return false;
-  });
-});
+// map stuffs
 
-function stripeResponseHandler(status, response) {
-  console.log(response);
-  // Grab the form:
-  var $form = $('#payment-form');
-  if (response.error) { // Problem!
-    // Show the errors on the form:
-    $form.find('.payment-errors').text(response.error.message);
-    $form.find('.submit').prop('disabled', false); // Re-enable submission
-  } else { // Token was created!
-    // Get the token ID:
-    var token = response.id;
-    $('#processing-notice').css("display","block");  
-    // Insert the token ID into the form so it gets submitted to the server:
-    $form.append($('<input type="hidden" name="stripeToken">').val(token));
-    // Submit the form:
-    $form.get(0).submit();
+function initMap() {
+  // style array
+  var styles = [
+    {
+      stylers: [
+        { hue: "#F26A62" },
+        { saturation: -20 }
+      ]
+    },{
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        { lightness: 100 },
+        { visibility: "simplified" }
+      ]
+    },{
+      featureType: "road",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]
+    }
+  ];
+  // create new styled map instance
+  var styledMap = new google.maps.StyledMapType(styles,
+    {name: "WDI Conf"});
+
+// Create a map object, and include the MapTypeId to add
+// to the map type control.
+var mapOptions = {
+  zoom: 14,
+  center: new google.maps.LatLng(-37.8352978, 144.9887219),
+  mapTypeControlOptions: {
+    mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
   }
 };
+var map = new google.maps.Map(document.getElementById('map'),
+  mapOptions);
+
+//Associate the styled map with the MapTypeId and set it to display.
+map.mapTypes.set('map_style', styledMap);
+map.setMapTypeId('map_style');
+}
